23c8826e5e073e706b101372a0dd20f0c086ee36
jakegatsby <jakegatsby@example.com>
2024-06-18 22:15:44 -0400

    images in carousel working

--- .museum/src/components/Carousel.vue
+++ .museum/src/components/Carousel.vue
@@ -1,15 +1,11 @@
 <script setup>
-  import { ref, defineProps } from 'vue';
-  const props = defineProps(['artifact']);
+  import { ref } from 'vue';
+  const props = defineProps(['images']);
+  const images = ref(props.images);
 </script>
 
 <template>
   <div class="carousel" data-flickity='{ "imagesLoaded": true, "percentPosition": false, "wrapAround": true, "pageDots": true, "prevNextButtons": false }'>
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" alt="orange tree" />
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" alt="submerged" />
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" alt="look-out" />
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" alt="One World Trade" />
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" alt="drizzle" />
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" alt="cat nose" />
+    <img v-for="image in images" :src="image" />
   </div>
 </template>
